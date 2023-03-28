47bc7747cdff734e5db4927ed44ce46ec757ed6f
925912615 <51504778+925912615@users.noreply.github.com>
2023-03-28 19:18:43 +0800

    Add files via upload
    init

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import Rect from './shape/Rect';
+import drawiBoardBagua from './tools/drawiBoard-bagua';
+declare const _default: {
+    drawiBoardBagua: typeof drawiBoardBagua;
+    Rect: typeof Rect;
+};
+export default _default;
