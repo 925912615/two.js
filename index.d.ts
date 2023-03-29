8652e878720c4a88c52d46dd29203b4aed6992ec
谛听 <2733808609@qq.com>
2023-03-29 15:28:19 +0800

    测试

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import Rect from './shape/Rect';
-import drawiBoardBagua from './tools/drawiBoard-bagua';
-declare const _default: {
-    drawiBoardBagua: typeof drawiBoardBagua;
-    Rect: typeof Rect;
-};
-export default _default;
